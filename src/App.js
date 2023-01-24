126cd4a5e7ba6e9bc4c02578b27a0e2cde234552
tommygomez25 <maysan54321@gmail.com>
2023-01-24 18:43:44 +0000

    Section 4 - State: How to Change Your App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import 'bulma/css/bulma.css';
-import ProfileCard from "./ProfileCard";
+import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 import AlexaImage from './images/alexa.png'; // since alexa.png is less than 9.7 kb, gets inlined (it is converted to base 64)
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png'; // since siri.png is more than 9.7 kb, gets treated as separate file
 
+function getRandomAnimal() {
+  const animals = ['bird','cat','cow','dog','gator','horse'];
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
 function App() {
+
+  const [animals,setAnimals] = useState([]);
+
+  const handleClick = () => {   
+  // isto modifica uma piece of state -> animals.push(getRandomAnimal())
+    setAnimals([...animals,getRandomAnimal()]);
+  };
+
+  const renderedAnimals = animals.map((animal,index) => {
+    return <AnimalShow type = {animal} key = {index} />
+  });
+
   return (
-    <div>
-      <section className='hero is-primary'>
-        <div className='hero-body'>
-          <p className='title'>Personal Digital Assistants</p>
-        </div>
-      </section>
-      <div className='container'>
-        <section className='section'>
-          <div className='columns'>
-            <div className='column is-4'>
-              <ProfileCard title = "Alexa" handle = "@alexa99" image = {AlexaImage}
-              description = "Alexa was created by Amazon"/>
-            </div>
-            <div className='column is-4'>
-              <ProfileCard title = "Cortana" handle = "@cortana32" image = {CortanaImage}
-              description = "Cortana was made by Microsoft"/>
-            </div>
-            <div className='column is-4'>
-              <ProfileCard title = "Siri" handle = "@siri01" image = {SiriImage}
-              description = "Siri was created by Apple"/>
-            </div>
-          </div>
-        </section>
-      </div>
+    <div className='app'>
+      <button onClick={handleClick}>Add Animal</button>
+      <div className='animal-list'>{renderedAnimals}</div>
     </div>
   )
 }
