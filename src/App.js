e5eb33adb90371580987371a40b8b19c0ee7be9b
tommygomez25 <maysan54321@gmail.com>
2023-02-07 15:38:54 +0000

    Section 7 - Data Persistence with API Requests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import axios from 'axios';
 import 'bulma/css/bulma.css';
 //import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
@@ -8,26 +9,51 @@ function App() {
 
   const [books,setBooks] = useState([]);
 
-  const deleteBookById = (id) => {
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+
+    setBooks(response.data);
+  }
+
+  useEffect(() => {
+    fetchBooks();
+  },[]); // o segundo argumento pode ser nada, [], [counter] 
+        // no caso de nada -> a função é sempre chamada depois de um rerender
+        // no caso de [] -> a função só é chamada no primeiro render
+        // no caso de [counter] ->  a função é chamada depois de um rerender SÓ SE a variável counter mudar
+
+  const deleteBookById = async (id) => {
+    const response = await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {return book.id !== id;});
 
     setBooks(updatedBooks);
   }
 
-  const editBookById = (id,title) => {
+  const editBookById = async (id,title) => {
+    
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title:title
+    });
+
     const updatedBooks = books.map((book) => {
-      if (book.id === id) {return {...book,title:title}}
+      if (book.id === id) {return {...book,...response.data}}
       return book;
     });
 
     setBooks(updatedBooks);
   }
-  const createBook = (title) => {
-    console.log(title);
+
+  const createBook = async (title) => {
     // REACT assume que se um array antigo e um novo estão a apontar
     // para a mesma posição de memória então não é preciso dar rerender
     // POR ISSO, não se pode dar simplesmente books.push()... 
-    const updatedBooks = [...books,{id:Math.round(Math.random() * 9999), title:title}];
+
+    const response = await axios.post('http://localhost:3001/books', {
+      title:title
+    });
+
+    const updatedBooks = [...books,response.data];
     
     setBooks(updatedBooks);
   };
